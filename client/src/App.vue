03627b1d97c394d0d558ee5432d91a63b2cdd3c4
Undroy <undyingshankha@live.com>
2019-07-26 01:49:48 +0530

    New Design Nav Drawer

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,33 +1,24 @@
 <template>
   <v-app>
-    <v-app-bar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        text
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
-    </v-app-bar>
-
+    <!-- <v-app-bar app> -->
+      <NavDrawer/>
+    <!-- </v-app-bar> -->
     <v-content>
       <HelloWorld/>
+      <!-- <router-view/> -->
     </v-content>
   </v-app>
 </template>
 
 <script>
+import NavDrawer from './components/share/NavDrawer'
 import HelloWorld from './components/HelloWorld';
 
 export default {
   name: 'App',
   components: {
     HelloWorld,
+    NavDrawer
   },
   data: () => ({
     //
---
+++ client/src/components/share/NavDrawer.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <v-toolbar :src="bg ? 'https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg' : undefined">
+      <v-toolbar-title>Title</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-toolbar-items>
+        <v-btn text>Link 1</v-btn>
+        <v-btn text>Link 2</v-btn>
+        <v-btn text>Link 3</v-btn>
+      </v-toolbar-items>
+
+      <template v-if="$vuetify.breakpoint.smAndUp">
+        <v-btn icon>
+          <v-icon>mdi-export-variant</v-icon>
+        </v-btn>
+        <v-btn icon>
+          <v-icon>mdi-delete-circle</v-icon>
+        </v-btn>
+        <v-btn icon>
+          <v-icon>mdi-plus-circle</v-icon>
+        </v-btn>
+      </template>
+    </v-toolbar>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    bg: true,
+    menu: false,
+    drawer: false,
+    items: [
+      { title: 'Edit Profile' },
+      { title: 'Logout' }
+    ]
+  }),
+  props: {
+      source: String
+  },
+//   methods: {
+//     referTo (route) {
+//         this.$router.push(route)
+//     },
+//     logout (route) {
+//       this.$store.state.isUserLoggedIn = false
+//       this.$store.state.token = null
+//       this.$store.state.loginuser = null
+//       this.$router.push(route)
+//     },
+//     droproute (data) {
+//       if (data === 'Logout') {
+//         this.logout ({name: 'home'})
+//       } else {
+//         alert (data)
+//       }
+//     }
+//   }
+}
+</script>
+
